154ffc91bf86d940738920ee56ad3709425b61f2
Alan Nakari <alan.nakari@revise.fi>
2022-06-23 13:52:05 +0300

    Update unifaun

--- unifaun_odoo_integration/models/unifaun_pickup_address.py
+++ unifaun_odoo_integration/models/unifaun_pickup_address.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 class PickupAddress(models.Model):
     _name = 'unifaun.pickup.address'
     _description = "Unifaun Pickup Address Details"
-    _rec_name = 'agent_name'
 
     shipping_provider = fields.Char(string='Shipping Provider')
     carrier = fields.Char(string='Carrier')
@@ -19,11 +18,5 @@ class PickupAddress(models.Model):
     agent_state = fields.Char(string='State')
     agent_country = fields.Char(string='Country')
     agent_phone = fields.Char(string='Phone')
-    agent_email = fields.Char(string='Email')
 
     picking_id = fields.Many2one('stock.picking', string='Stock')
-    sale_id = fields.Many2one('sale.order', string='Sale Order', copy=False)
-
-    def set_location(self):
-        self.ensure_one()
-        self.sale_id.unifaun_location_id = self.id
--- unifaun_shipping_integration/models/stock_package_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class StockPackageType(models.Model):
-    _inherit = 'stock.package.type'
-
-    package_carrier_type = fields.Selection(selection_add=[('unifaun', 'Unifaun')])
-    unifaun_package_code = fields.Char(string='Unifaun Package Code')
